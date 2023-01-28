d07eb890b21221f244e2b22f4887c69ac4eb6eda
hhw12409 <sksk2425@naver.com>
2023-01-29 00:47:52 +0900

    feat: Header setting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import styles from '@/styles/header.module.scss';
+import Image from 'next/image';
 
 const Header = () => {
-  return null
-}
+  return (
+    <header className={styles.header}>
+      <div className={styles.flexItem}>
+        <Link href="/" className={styles.box}>
+          <Image
+            src={'https://lecture-1.vercel.app/inflearn.png'}
+            alt="Logo"
+            width={110}
+            height={20}
+          />
+        </Link>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
